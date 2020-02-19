1d3805c44d5f5d8f908461950ee3af579acbd751
Vladana <vladana.pandurevic@innovic.io>
2020-02-19 13:08:55 +0100

    Merge branch 'master' of /home/innovic/WebstormProjects/memory-game with conflicts.

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const numberOfCubes = 36;
+    const numberOfCubes = 36;
 
-    // Vladana
     function calculateGameContainerWidthBasedOnHeight() {
         const height = window.innerHeight;
-      //  document.write(height);
-       return {
+
+        return {
             width: height,
             height: height
         }
     }
-    // Milana
+
     function calculateWidthAndHeightForCubes(width, height, numberOfCubes) {
         const widthOfOneCube = width / Math.sqrt(numberOfCubes);
         const heightOfOneCube = height / Math.sqrt(numberOfCubes);
-
 
         return {
             width: widthOfOneCube,
@@ -26,30 +24,28 @@ document.addEventListener("DOMContentLoaded", () => {
     const gameContainer = calculateGameContainerWidthBasedOnHeight();
     const cube = calculateWidthAndHeightForCubes(gameContainer.width, gameContainer.height, numberOfCubes);
 
-  const gameElement = document.getElementById("game");
+    const gameElement = document.getElementById("game");
 
-    gameElement.style.width = gameContainer.width +"px";
-   gameElement.style.height= calculateGameContainerWidthBasedOnHeight().height +"px";
+    gameElement.style.width = gameContainer.width + "px";
+    gameElement.style.height = gameContainer.height + "px";
 
 
-  function generateCubsInGameElement(cube, numberOfCubes) {
+    function generateCubsInGameElement(cube, numberOfCubes) {
 
-    for (let i = 0; i < numberOfCubes; i++) {
-      const card = document.createElement("div");
-      card.innerText = i.toString();
+        for (let i = 0; i < numberOfCubes; i++) {
+            const card = document.createElement("div");
+            //card.innerText = "This is a paragraph";
+            card.style.background = "red";
+            card.style.height = cube.height + "px";
+            card.style.width = cube.width + "px";
+            card.style.display = "inline-block";
+            //card.style.border= "0.1px dashed black";
 
-      // Milana
-      // Calculate font size and place text in middle
-
-      card.style.background = "red";
-      card.style.height = cube.height + "px";
-      card.style.width = cube.width + "px";
-      card.style.display = "inline-block";
-
-    gameElement.appendChild(card);
-}
+            gameElement.appendChild(card);
+        }
     }
-     generateCubsInGameElement(cube, numberOfCubes);
+
+    generateCubsInGameElement(cube, numberOfCubes);
 
 });
 
