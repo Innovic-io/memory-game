41c0f820a31f1316c41098ebc4a47a972280003d
Kristina <ilic.kristina1996@gmail.com>
2020-02-19 11:05:06 +0100

    Merge branch 'master' of /home/innovic/WebstormProjects/memory-game with conflicts.

--- app.js
+++ app.js
@@ -25,13 +25,35 @@ document.addEventListener("DOMContentLoaded", () => {
     const gameElement = document.getElementById("game");
     // Kristina
     // Set on gameElement width and height
+   // gameElement.style.width = "90px";
+   // gameElement.style.height= "90px";
+    gameElement.style.width = "gameContainer.width";
+    gameElement.style.height= "gameContainer.height";
 
     // Kristina
+    // function generateCubsInGameElement(cube, numberOfCubes) {
+    //
+    //         gameElement.appendChild(
+    //             document.createElement("div")
+    //         )
+    //
+    // }
     function generateCubsInGameElement(cube, numberOfCubes) {
-        // gameElement.appendChild(
-        //     document.createElement("div")
-        // )
+
+for( let i =0; i<36; i++) {
+    const card = document.createElement("div");
+    card.innerText = "This is a paragraph";
+    card.style.background = "red";
+    card.style.height = "90px";
+    card.style.width = "90px";
+    card.style.display=  "inline-block";
+    card.style.border= "1px solid black";
+
+    gameElement.appendChild(card);
+}
     }
+     generateCubsInGameElement();
+
 });
 
 
