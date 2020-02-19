8d6268cd0560cbea43259620357223ebabe456e1
Ivana Fustar <ivanafustar@hotmail.com>
2020-02-19 10:03:04 +0100

    Tasks

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
-
 document.addEventListener("DOMContentLoaded", () => {
+    const numberOfCubes = 25;
 
-    const game = document.getElementById("game");
-    console.log(game)
+    // Vladana
+    function calculateGameContainerWidthBasedOnHeight() {
+        // window.innerWidth
+        // window.innerHeight
+        return {
+            width: 100,
+            height: 100
+        }
+    }
+
+    // Milana
+    function calculateWidthAndHeightForCubes(width, height, numberOfCubes) {
+
+        return {
+            width: 100,
+            height: 100
+        }
+    }
+
+    const gameContainer = calculateGameContainerWidthBasedOnHeight();
+    const cube = calculateWidthAndHeightForCubes(gameContainer.width, gameContainer.height, numberOfCubes);
+
+    const gameElement = document.getElementById("game");
+    // Kristina
+    // Set on gameElement width and height
+
+    // Kristina
+    function generateCubsInGameElement(cube, numberOfCubes) {
+        // gameElement.appendChild(
+        //     document.createElement("div")
+        // )
+    }
 });
 
 
