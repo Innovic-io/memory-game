e74b253eecbd65b73ba60067d388fc567642ca6e
innovic <VladanaP09>
2020-02-19 11:23:29 +0100

    Merge branch 'master' of /home/innovic/WebstormProjects/memory-game with conflicts.

--- app.js
+++ app.js
@@ -1,47 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
   const numberOfCubes = 36;
 
-  function calculateGameContainerWidthBasedOnHeight() {
-
-    return {
-      width: window.innerHeight,
-      height: window.innerHeight
+    // Vladana
+    function calculateGameContainerWidthBasedOnHeight() {
+        const height = window.innerHeight;
+      //  document.write(height);
+       return {
+            width: height,
+            height: height
+        }
     }
-  }
+    // Milana
+    function calculateWidthAndHeightForCubes(width, height, numberOfCubes) {
+        const widthOfOneCube = width / Math.sqrt(numberOfCubes);
+        const heightOfOneCube = height / Math.sqrt(numberOfCubes);
 
-  function calculateWidthAndHeightForCubes(width, height, numberOfCubes) {
-    const widthOfOneCube = width / Math.sqrt(numberOfCubes);
-    const heightOfOneCube = height / Math.sqrt(numberOfCubes);
 
-    return {
-      width: widthOfOneCube,
-      height: heightOfOneCube
+        return {
+            width: widthOfOneCube,
+            height: heightOfOneCube
+        }
+
     }
-  }
 
-  function calculateCubeFontSize(cubeHeight) {
-
-    return 12312312;
-  }
-
-  // Vladana
-  function generateRandomColor(numberOfCubes) {
-    // Return map where keys are from 0 to 17, and values are random colors
-  }
-
-  // Kristina
-  function generateArrayOfTileValues(numberOfCubes) {
-    // Return array with length of numberOfCubes, where we show duplicate numbers to numberOfCubes / 2
-  }
-
-  const gameContainer = calculateGameContainerWidthBasedOnHeight();
-  const cube = calculateWidthAndHeightForCubes(gameContainer.width, gameContainer.height, numberOfCubes);
-  const fontSize = calculateCubeFontSize(cube.height);
+    const gameContainer = calculateGameContainerWidthBasedOnHeight();
+    const cube = calculateWidthAndHeightForCubes(gameContainer.width, gameContainer.height, numberOfCubes);
 
   const gameElement = document.getElementById("game");
 
-  gameElement.style.width = gameContainer.width + "px";
-  gameElement.style.height = gameContainer.height + "px";
+    gameElement.style.width = gameContainer.width +"px";
+   gameElement.style.height= calculateGameContainerWidthBasedOnHeight().height +"px";
+
 
   function generateCubsInGameElement(cube, numberOfCubes) {
 
@@ -57,13 +46,10 @@ document.addEventListener("DOMContentLoaded", () => {
       card.style.width = cube.width + "px";
       card.style.display = "inline-block";
 
-      gameElement.appendChild(card);
+    gameElement.appendChild(card);
+}
     }
-  }
-
-  generateCubsInGameElement(cube, numberOfCubes);
-
-
+     generateCubsInGameElement(cube, numberOfCubes);
 
 });
 
