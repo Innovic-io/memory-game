23a3e14dd5a71f4c97dfda5d04396d786f36442e
Kristina <ilic.kristina1996@gmail.com>
2020-02-21 08:33:02 +0100

    logic

--- memory-guess/app.js
+++ memory-guess/app.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const isMobile = isMobileDevice();
   const numberOfCubes = isMobile ? 36 : 64;
   const cardFlipped = [];
+  const cardFlippedInteger = [];
   let numberToGuess = [];
   const duplicateNumberToGuess = [];
   let level = 1;
@@ -60,9 +61,11 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  function isMobileDevice () {
+  function isMobileDevice() {
     let check = false;
-    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    (function (a) {
+      if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
+    })(navigator.userAgent || navigator.vendor || window.opera);
     return check;
   }
 
@@ -120,7 +123,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function appendClickEventOnCube(card, index) {
     card.addEventListener('click', () => {
-      console.log(index);
+      flipCube(card, index);
+      appendToFlippedArrayAndCheckIfTheyAreSame(card);
     });
   }
 
@@ -134,9 +138,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function flipAllCards() {
 
-    const card =  document.getElementsByClassName("box");
-    for(let i = 0; i< card.length; i++)
-    {
+    const card = document.getElementsByClassName("box");
+    for (let i = 0; i < card.length; i++) {
       card[i].style.background = "lightseagreen";
       card[i].innerText = "?";
     }
@@ -155,22 +158,36 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function appendToFlippedArrayAndCheckIfTheyAreSame(card) {
-    if (cardFlipped.length === 0) {
-      cardFlipped.push(card);
-      return;
-    }
-    if (cardFlipped.length === 1 && cardFlipped.indexOf(card) === -1) {
-      cardFlipped.push(card);
-      if (cardFlipped[0].innerText === cardFlipped[1].innerText) {
-        markCardAsResolved(cardFlipped[0]);
-        markCardAsResolved(cardFlipped[1]);
-      } else {
-        restartCard(cardFlipped[0]);
-        restartCard(cardFlipped[1]);
+
+    for (let i = 1; i <= numberOfCubes / 2; i++) {
+      if (level === i) {
+        if (cardFlipped.length < level * 2) {
+          cardFlipped.push(card.innerText);
+          return;
+        }
+        for (const element of cardFlipped) {
+          if (cardFlippedInteger.length < level * 2) {
+            cardFlippedInteger.push(parseInt(element));
+          }
+        }
+        const uniqSortedCardFlippedInteger = [...new Set(cardFlippedInteger)].sort((a, b) => a - b);
+        console.log(cardFlippedInteger);
+        console.log(uniqSortedCardFlippedInteger);
+        const sortedNumberToGuess = numberToGuess.sort((a, b) => a - b);
+        console.log(sortedNumberToGuess);
+        if (sortedNumberToGuess.length !== uniqSortedCardFlippedInteger.length) {
+          return console.log(false);
+        }
+        for (let j = 0; j < uniqSortedCardFlippedInteger.length; ++j) {
+          if (sortedNumberToGuess[j] !== uniqSortedCardFlippedInteger[j])
+            return console.log(false);
+
+        }
+        return console.log(true);
+
       }
 
-      cardFlipped.length = 0;
-      return false;
+
     }
   }
 
@@ -178,8 +195,8 @@ document.addEventListener("DOMContentLoaded", () => {
     for (let i = 0; i < numberOfCubes; i++) {
       const card = document.createElement("div");
       appendCustomStylesToElement(card, cube, i);
-
       gameElement.appendChild(card);
+      appendClickEventOnCube(card, i);
     }
   }
 
@@ -198,8 +215,10 @@ document.addEventListener("DOMContentLoaded", () => {
   start();
   startTimer();
   countdown();
-  setTimeout(()=>{flipAllCards()}, 10000);
-  
+  setTimeout(() => {
+    flipAllCards()
+  }, 3000);
+
 });
 
 
