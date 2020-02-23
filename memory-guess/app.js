faf7e2654d8f853cfc6a9fd1e996ca670788ed66
Vladana <vladana.pandurevic@innovic.io>
2020-02-23 18:47:20 +0100

    New game timer and fixed bugs.

--- memory-guess/app.js
+++ memory-guess/app.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const isMobile = isMobileDevice();
   const numberOfCubes = isMobile ? 36 : 64;
   const cardFlippedIndex = [];
+  const seconds = 10;
   let level = 1;
   let startDate = null;
   let interval;
-  const seconds = 10;
   let milliseconds = seconds * 1000;
   let timer;
 
@@ -42,9 +42,22 @@ document.addEventListener("DOMContentLoaded", () => {
     }, 1000);
   }
 
+  function gameTimer() {
+    let secondsRemaining = seconds * level;
+    clearInterval(timer);
 
+    timer = setInterval(() => {
+      secondsRemaining--;
+      document.getElementById("countdown").innerHTML = "00:00:" + (secondsRemaining < 10 ? "0" + secondsRemaining : secondsRemaining);
+      if (secondsRemaining <= 0) {
+        drawGameOver();
+        clearInterval(timer);
+        clearInterval(interval);
+      }
+    }, 1000);
+  }
+  
   function calculateGameContainerWidthBasedOnHeight() {
-
     return {
       width: isMobile ? window.innerWidth : window.innerHeight,
       height: isMobile ? window.innerWidth : window.innerHeight
@@ -66,6 +79,7 @@ document.addEventListener("DOMContentLoaded", () => {
     (function (a) {
       if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
     })(navigator.userAgent || navigator.vendor || window.opera);
+
     return check;
   }
 
@@ -95,17 +109,20 @@ document.addEventListener("DOMContentLoaded", () => {
       array[arrayLength] = array[index];
       array[index] = temp;
     }
+
     return array;
   }
 
   function generateArrayOfTileValues(numberOfCubes, shouldDuplicate = true) {
     const arrayToRepeat = [];
+
     for (let i = 1; i <= numberOfCubes / 2; i++) {
       arrayToRepeat.push(i);
     }
 
     const arrayLength = shouldDuplicate ? 2 : 1;
     const arrayOfNumbers = [].concat(...Array(arrayLength).fill(arrayToRepeat));
+
     return shuffle(arrayOfNumbers);
   }
 
@@ -138,7 +155,6 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-
   function flipCube(card, cardIndex) {
     const value = tileValues[cardIndex];
     const color = colors.get(value);
@@ -166,10 +182,10 @@ document.addEventListener("DOMContentLoaded", () => {
     if (a == null || b == null) return false;
     if (a.length != b.length) return false;
 
-
     for (let i = 0; i < a.length; ++i) {
       if (a[i] !== b[i]) return false;
     }
+
     return true;
   }
 
@@ -184,8 +200,7 @@ document.addEventListener("DOMContentLoaded", () => {
     return false;
   }
 
-  function restartGame() {
-   // generateCubsInGameElement(cube, numberOfCubes);
+  function generateNewLevel() {
     cardFlippedIndex.length = 0;
 
     setTimeout(() => {
@@ -196,7 +211,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
       setTimeout(() => {
         flipAllCards();
-        countdown();
+        gameTimer();
       }, level * milliseconds)
     }, 200)
   }
@@ -205,22 +220,22 @@ document.addEventListener("DOMContentLoaded", () => {
     if (cardFlippedIndex.length < level * 2) {
       cardFlippedIndex.push(cardIndex);
       const isDone = checkIfResultIsCorrect();
-      // const cards = document.getElementsByClassName("box");
+
       if (cardFlippedIndex.length === level * 2) {
 
         if (isDone) {
           level += 1;
         } else {
           level = 1;
+          flipAllCards();
           drawGameOver();
           clearInterval(timer);
           clearInterval(interval);
-
           return;
         }
-        restartGame();
-
+        generateNewLevel();
       }
+
     }
   }
 
@@ -229,10 +244,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const lettersOver = ['O', 'V', 'E', 'R'];
     const playSymbol = ['▶'];
     const cards = document.getElementsByClassName("box");
-    for( let i =0; i< cards.length; i++) {
+
+    for (let i = 0; i < cards.length; i++) {
       cards[i].classList.add("notAllowedClick");
     }
+
     let startPosition = calculateStartPosition(1);
+
     for (const letter of lettersGame) {
       drawGameOverCards(cards[startPosition], letter, "red");
       startPosition++;
@@ -251,6 +269,7 @@ document.addEventListener("DOMContentLoaded", () => {
       play();
       numberToGuess = generateNumberToGuess(tileValues);
     });
+
     for (const letter of playSymbol) {
       drawGameOverCards(cards[startPosition], letter, "green");
       startPosition++;
@@ -272,11 +291,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function drawPlay() {
     flipAllCards();
-
     const lettersStart = ['P', 'L', 'A', 'Y'];
     const playSymbol = ['▶'];
     const cards = document.getElementsByClassName("box");
-    for( let i =0; i< cards.length; i++) {
+
+    for (let i = 0; i < cards.length; i++) {
       cards[i].classList.add("notAllowedClick");
     }
 
@@ -303,26 +322,24 @@ document.addEventListener("DOMContentLoaded", () => {
   function play() {
     generateCubsInGameElement(cube, numberOfCubes);
     cardFlippedIndex.length = 0;
+
     start();
     startTimer();
     countdown();
-
     setTimeout(() => {
       flipAllCards();
-      countdown();
+      gameTimer();
     }, milliseconds * level);
-
   }
 
   function generateCubsInGameElement(cube, numberOfCubes) {
     gameElement.innerHTML = "";
+
     for (let i = 0; i < numberOfCubes; i++) {
       const card = document.createElement("div");
       appendCustomStylesToElement(card, cube, i);
       appendClickEventOnCube(card, i);
       document.getElementById("level").innerHTML = "Level: " + level;
-
-
       gameElement.appendChild(card);
     }
 
@@ -341,7 +358,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   generateCubsInGameElement(cube, numberOfCubes);
   drawPlay();
-
 });
 
 
